4232b9d63db4f170aac7944ce0062c18f637abc5
Cory Noll Golden <cory190@live.com>
2018-05-13 21:56:59 -0600

    0.1.1! Improved code comments and made minor adjustments to the code.

--- checkpassword.py
+++ checkpassword.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-print "Created by Cory Noll (Null) Crimmins - Golden on May 13 14:08:04 MDT 2018
+print "Created by Cory Noll (Null) Crimmins - Golden on May 12 08:20:32 MDT 2018"
 print "Voyant assignment #2"
 print "MIT License"
 
@@ -10,58 +10,61 @@ class RadixNode:
     # Radix Trie node
     def __init__(self):
         # Constructor
-        self.kids = {}
-        self.leaf = False
+        self.kids = {} # Table container
+        self.leaf = False # A leaf is an ending to a trie branch
 
 class RadixTrie:
     # Radix trie or tree class
     def __init__(self):
         # Constructor
-        self.root = RadixNode()
+        self.root = RadixNode() # "The trunk"
 
     def insert(self, key):
-        # Traverses the tree and finds a nice place to put a node
+        # Traverses the tree for each char and adds nodes to the table.
         table = self.root
-        for i in range(0, len(key)):
-            c = key[i]
-            if c not in table.kids:
-                table.kids.update({c: RadixNode()})
-            if i == (len(key) - 1):
+        keyLen = len(key)
+        for i in range(0, keyLen):
+            c = key[i] # Character from key
+            if c not in table.kids: # If not in table, add
+                table.kids[c] = RadixNode() # Create node
+            if i == (keyLen - 1): # Are we at the end... do we leaf?
                 table.kids[c].leaf = True
-            table = table.kids[c]
+            table = table.kids[c] # Done... next char
 
     def remove(self, key):
-        # Traverses the tree and removes a node.
-        stack = [self.root]
+        # This was totally extra and I didn't use it once...
+        # Traverses the tree for each char and trims nodes for each match.
+        stack = [self.root] # We need to get the path
         table = self.root
-        for i in range(0, len(key)):
-            c = key[i]
-            if c in table.kids:
-                table = table.kids[c]
-                stack += [table]
+        keyLen = len(key)
+        for i in range(0, keyLen):
+            c = key[i] # Character from key
+            if c in table.kids: # Match!
+                stack += [table] # Push to stack
+                table = table.kids[c] # Next char
             else:
-                return False
-        if stack[-1].leaf == True:
-            stack[-1].leaf = False
-            for i in range(0, len(key)):
-                x = len(key) - i - 1
-                c = key[x]
-                del stack[x].kids[c]
+                return False # Not found...
+        if stack[-1].leaf == True: # Leaf?
+            stack[-1].leaf = False # Not anymore!
+            for i in range(0, keyLen):
+                x = len(key) - i - 1 # Index from depth
+                del stack[x].kids[key[x]] # Removed
         return True
 
     def contains(self, key):
-        # Traverses the tree and finds a match.
+        # Traverses the tree and finds a match that is a leaf.
         table = self.root
-        for i in range(0, len(key)):
-            c = key[i]
-            if i == (len(key)-1):
-                return c in table.kids and table.kids[c].leaf
+        keyLen = len(key)
+        for i in range(0, keyLen):
+            c = key[i] # Character from key
+            if i == (keyLen - 1): # Are we at the end?
+                return c in table.kids and table.kids[c].leaf # Match!
             else:
-                if c in table.kids:
-                    table = table.kids[c]
+                if c in table.kids: # Found?
+                    table = table.kids[c] # Next!
                 else:
-                    return False
-        return False
+                    return False # Darn.
+        return False # Darn.
 
 argLength = len(sys.argv)-1 # We don't care about the script location
 script = sys.argv[0] # That last line was kinda a lie... we do...
@@ -75,7 +78,7 @@ else:
     file = open("1000MostCommonPasswords(byDavidWittman).txt", "r")
     tree = RadixTrie()
     for line in file.readlines():
-        tree.insert(line[:-1]) # removes the newline
+        tree.insert(line[:-1]) # [:-1] removes the newline
     file.close()
     if tree.contains(password):
         print "Common password"
